62544e5ccb4e0e3e7c0cdaef1f7174feeb38acfb
Arax <mohammadmahdi.zaeimi@mail.concordia.ca>
2024-07-01 14:50:42 -0400

    form with hook form

---
+++ src/components/FormWithHookForm.tsx
@@ -0,0 +1,44 @@
+import { FormEvent } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+export const FormWithHookForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* 
+        This trick will create the markups easier for us.
+        div.mb-3>label.form-lable+input.form-control 
+      */}
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
